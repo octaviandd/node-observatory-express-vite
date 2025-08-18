383ff09914e4da4c99b803ae76a4ba22f6f43f4c
Octavian <odavid@castlefineart.com>
2025-08-18 14:39:05 +0100

    [master] - Update UI:

--- packages/ui/src/store.tsx
+++ packages/ui/src/store.tsx
@@ -1,19 +1,15 @@
 /** @format */
 
 import React, { createContext, useReducer, ReactNode, Dispatch } from "react";
-
-// Define the shape of your state
 interface State {
   period: "1h" | "24h" | "7d" | "14d" | "30d";
 }
 
-// Define the shape of your actions
 type Action = {
   type: "setPeriod";
   payload: "1h" | "24h" | "7d" | "14d" | "30d";
 };
 
-// Create the initial state
 const initialState: State = {
   period: (window.localStorage.getItem("period") as
     | "1h"
@@ -23,7 +19,6 @@ const initialState: State = {
     | "30d") ?? '1h'
 };
 
-// Create a reducer function
 const reducer = (state: State, action: Action): State => {
   switch (action.type) {
     case "setPeriod":
@@ -33,7 +28,6 @@ const reducer = (state: State, action: Action): State => {
   }
 };
 
-// Create the context
 const StoreContext = createContext<{
   state: State;
   dispatch: Dispatch<Action>;
@@ -42,7 +36,6 @@ const StoreContext = createContext<{
   dispatch: () => undefined,
 });
 
-// Create a provider component
 const StoreProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
